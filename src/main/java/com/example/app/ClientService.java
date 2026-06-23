3418fc28ee7d10bba88453a84b6af2fcf81211de
Mr Hitesh Namrani <hiteshnamrani@gmail.com>
2026-06-23 17:34:03 +0530

    fixed typos and indentation

--- src/main/java/com/example/app/ClientService.java
+++ src/main/java/com/example/app/ClientService.java
@@ -1,35 +1,38 @@
 package com.example.app;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.services;
-@Services
-  public class ClientService{
+import org.springframework.stereotype.Service;
 
-  @Autowired private clientRepository;
-  public Client Registerclient(String username,String rawPassword){
-    if(repository.findByl
-       d(username).isPresent())
-      {
-      throw new
-        RuntimeException("Username is already taken!");
+@Service
+public class ClientService {
+
+  @Autowired
+  private ClientRepository repository;
+
+  public Client registerClient(String username, String rawPassword) {
+    if (repository.findById(username).isPresent()) {
+      throw new RuntimeException("Username is already taken!");
     }
-    Client client = new client(username,rawPassword);
+    Client client = new Client(username, rawPassword);
     return repository.save(client);
-}
-  public Client verifyLogin(String username, String password){
-    Client client = repository.findBy(username).orElseThrow(() ->new Runtime Expection ("client not found!");
   }
-return client;
-}
-publi Client
-  processTransactoion(String username,string password,Double amount{
 
-Client client = verifyLogin (username,password);
-double newBalance = client.getBalance()+amount;
-if(newBalance<0){
-  throw new 
-    RuntimeException("Insufficient funds!
-                     ");
+  public Client verifyLogin(String username, String password) {
+    Client client = repository.findById(username)
+        .orElseThrow(() -> new RuntimeException("Client not found!"));
+    if (!client.checkPassword(password)) {
+      throw new RuntimeException("Incorrect password!");
+    }
+    return client;
+  }
+
+  public Client processTransaction(String username, String password, Double amount) {
+    Client client = verifyLogin(username, password);
+    double newBalance = client.getBalance() + amount;
+    if (newBalance < 0) {
+      throw new RuntimeException("Insufficient funds!");
+    }
+    client.setBalance(newBalance);
+    return repository.save(client);
+  }
 }
-  client.setBalance(newBalance);
-  return repository.save(client);
-  }}
