b19ff584722f87597c919b694f7f1df799bd1f97
hitesh-namrani <hiteshnamrani@gmail.com>
2026-06-23 15:03:18 +0530

    Made Client.java to be stored as an entity in DB

--- src/main/java/com/example/app/Client.java
+++ src/main/java/com/example/app/Client.java
@@ -1,4 +1,31 @@
 package com.example.app;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private Double balance;
+
+    protected Client() {}
+
+    public Client(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.balance = 0.0;
+    }
+
+    public String getUsername() { return username; }
+    public Double getBalance() { return balance; }
+    public void setBalance(Double balance) { this.balance = balance; }
+
+    public boolean checkPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
 }
