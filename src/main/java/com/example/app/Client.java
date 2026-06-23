2de0f5a3717b39e5f16a475cfc669d63cc74ff66
Yashi6398 <yashi.thakur@pinelabs.com>
2026-06-23 14:11:20 +0530

    Refactor Client class with properties and methods

--- src/main/java/com/example/app/Client.java
+++ src/main/java/com/example/app/Client.java
@@ -1,4 +1,72 @@
 package com.example.app;
 
-public class Client {
+
+
+  public class Client {
+
+    private int id;
+    private String username;
+    private String password;
+    private double balance;
+
+    public Client(int id, String username, String password, double balance) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
+  public static void main(Strings, args[]){
+     public static void main(String[] args) {
+        Client client = new Client(
+                1,
+                "john123",
+                "password123",
+                5000.0
+        );
+
+        System.out.println(client);
+    }
+    
+  }
+  
+
