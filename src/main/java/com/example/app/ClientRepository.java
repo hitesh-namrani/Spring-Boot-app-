99407d4d42f4e2a24fb3ffa3386161d8745f445d
Yashi6398 <yashi.thakur@pinelabs.com>
2026-06-23 16:54:43 +0530

    Fix typo in ClientRepository interface declaration

--- src/main/java/com/example/app/ClientRepository.java
+++ src/main/java/com/example/app/ClientRepository.java
@@ -1,6 +1,6 @@
 package com.example.app;
 import org.springframework.data.jpa.repository.JpaRepository;
-  public interface ClientRepository extends JpaRepositry<Client,String>{
+  public interface ClientRepository extends JpaRepository<Client,String>{
     
   }
 
