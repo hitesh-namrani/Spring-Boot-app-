dc8bce2a3392476212b4313563cbaa84f3174337
hitesh-namrani <hiteshnamrani@gmail.com>
2026-06-23 17:41:54 +0530

    made ClientController.java file and fixed indentation in WalletApplication.java

--- src/main/java/com/example/app/ClientController.java
+++ src/main/java/com/example/app/ClientController.java
@@ -1,4 +1,30 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @PostMapping("/register")
+    public Client register(@RequestParam String username, @RequestParam String password) {
+        return service.registerClient(username, password);
+    }
+
+    @PostMapping("/transaction")
+    public Client transact(
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam Double amount) {
+        return service.processTransaction(username, password, amount);
+    }
+
+    @GetMapping("/balance")
+    public Double getBalance(@RequestParam String username, @RequestParam String password) {
+        return service.verifyLogin(username, password).getBalance();
+    }
 }
