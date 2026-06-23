b7f697aac74c1f10d646b3284bf99f0e9ac68f03
Yashi6398 <yashi.thakur@pinelabs.com>
2026-06-23 16:45:32 +0530

    Fix WalletApplication class definition and main method

--- src/main/java/com/example/app/WalletApplication.java
+++ src/main/java/com/example/app/WalletApplication.java
@@ -1,5 +1,13 @@
 package com.example.app;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WalletApplication {
+  @SpringBootApplication
+  public class WalletApplication{
+  public static void main(String[] , args){
+    SpringApplication.run
+      (WalletApplication.class,args);
+  }
+  }
 
 }
