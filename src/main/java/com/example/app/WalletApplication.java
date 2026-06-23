f13e68feeb9ff662f021e58c0608d8e7529e1f5f
Yashi6398 <yashi.thakur@pinelabs.com>
2026-06-23 16:56:51 +0530

    Fix formatting and syntax in WalletApplication class

--- src/main/java/com/example/app/WalletApplication.java
+++ src/main/java/com/example/app/WalletApplication.java
@@ -1,13 +1,12 @@
 package com.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-public class WalletApplication {
+
   @SpringBootApplication
   public class WalletApplication{
-  public static void main(String[] , args){
-    SpringApplication.run
-      (WalletApplication.class,args);
+  public static void main(String[]  args){
+    SpringApplication.run(WalletApplication.class,args);
   }
   }
 
-}
+
